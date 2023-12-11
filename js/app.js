80346301f43c84f41db75faacce6b61ef4350cb5
andrew-focus-dev <andrew-focus-dev@proton.me>
2023-12-11 21:12:39 +0200

    responsive

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ new Swiper('#trusted-slider', {
   slidesPerView: 5,
   autoplay: { delay: 2500 },
   breakpoints: {
-    310: { slidesPerView: 1 },
-    420: { slidesPerView: 2 },
-    550: { slidesPerView: 3 },
+    300: { slidesPerView: 3, spaceBetween: 35 },
+    420: { slidesPerView: 3, spaceBetween: 15 },
     768: { slidesPerView: 4, autoplay: true },
     1050: { slidesPerView: 5 },
   },
@@ -16,5 +15,9 @@ new Swiper('#read-slider', {
   navigation: {
     nextEl: '.read-navigation__right',
     prevEl: '.read-navigation__left',
+  },
+  breakpoints: {
+    300: {spaceBetween: 0, slidesPerView: 1},
+    768: { spaceBetween: 15, slidesPerView: 2 },
   },
 })
